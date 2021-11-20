dd62c1c1fb0238435bf5e79d48705ae046b36013
NotCoffee418 <9306304+NotCoffee418@users.noreply.github.com>
2021-11-20 08:54:39 +0100

    VS Project Files

---
+++ TranqService/Program.cs
@@ -0,0 +1,10 @@
+using TranqService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
