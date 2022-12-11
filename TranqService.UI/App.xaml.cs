78a143acd0bcd69ae17767463571189d3f927566
NotCoffee418 <9306304+NotCoffee418@users.noreply.github.com>
2022-12-11 03:01:55 +0100

    Add crash catcher

--- TranqService.UI/App.xaml.cs
+++ TranqService.UI/App.xaml.cs
@@ -13,5 +13,14 @@ namespace TranqService.UI
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            Application.Current.DispatcherUnhandledException += Current_DispatcherUnhandledException;
+        }
+
+        private void Current_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {            
+            MessageBox.Show(e.Exception.Message, "TranqService.UI has crashed", MessageBoxButton.OK, MessageBoxImage.Error);            
+        }
     }
 }
